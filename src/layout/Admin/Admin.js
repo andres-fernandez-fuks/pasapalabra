e713929a13789057c8ea3e3fb838c97164c1779f
Andy <anfernandez@fi.uba.ar>
2022-07-19 15:16:26 -0300

    adds Admin layout

---
+++ src/layout/Admin/Admin.js
@@ -0,0 +1,128 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import { Route, Switch, Redirect, useLocation } from "react-router-dom";
+// javascript plugin used to create scrollbars on windows
+import PerfectScrollbar from "perfect-scrollbar";
+
+// core components
+import AdminNavbar from "components/Navbars/AdminNavbar.js";
+import Footer from "components/Footer/Footer.js";
+import Sidebar from "components/Sidebar/Sidebar.js";
+
+import routes from "utils/routes.js";
+
+import logo from "assets/img/logo.png";
+
+var ps;
+
+function Admin(props) {
+  const location = useLocation();
+  const mainPanelRef = React.useRef(null);
+  const [sidebarOpened, setsidebarOpened] = React.useState(
+    document.documentElement.className.indexOf("nav-open") !== -1
+  );
+  React.useEffect(() => {
+    if (navigator.platform.indexOf("Win") > -1) {
+      document.documentElement.className += " perfect-scrollbar-on";
+      document.documentElement.classList.remove("perfect-scrollbar-off");
+      ps = new PerfectScrollbar(mainPanelRef.current, {
+        suppressScrollX: true,
+      });
+      let tables = document.querySelectorAll(".table-responsive");
+      for (let i = 0; i < tables.length; i++) {
+        ps = new PerfectScrollbar(tables[i]);
+      }
+    }
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      if (navigator.platform.indexOf("Win") > -1) {
+        ps.destroy();
+        document.documentElement.classList.add("perfect-scrollbar-off");
+        document.documentElement.classList.remove("perfect-scrollbar-on");
+      }
+    };
+  });
+  React.useEffect(() => {
+    if (navigator.platform.indexOf("Win") > -1) {
+      let tables = document.querySelectorAll(".table-responsive");
+      for (let i = 0; i < tables.length; i++) {
+        ps = new PerfectScrollbar(tables[i]);
+      }
+    }
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    if (mainPanelRef.current) {
+      mainPanelRef.current.scrollTop = 0;
+    }
+  }, [location]);
+  // this function opens and closes the sidebar on small devices
+  const toggleSidebar = () => {
+    document.documentElement.classList.toggle("nav-open");
+    setsidebarOpened(!sidebarOpened);
+  };
+
+  React.useEffect(
+    function checkIfLoggedIn() {
+      if (window.location.pathname == simple_routes.login) {
+        return
+      } else if (!localStorage.getItem("token")) {
+        window.location.pathname = simple_routes.login;
+      }
+    }
+  )
+  
+  return (
+    <BackgroundColorContext.Consumer>
+      {({ color, changeColor }) => (
+        <React.Fragment>
+          <div className="wrapper">
+            <Sidebar
+              routes={routes}
+              logo={{
+                text: "ITIL Management",
+                imgSrc: logo,
+              }}
+              toggleSidebar={toggleSidebar}
+            />
+            <div className="main-panel"  data={color}>
+              <AdminNavbar
+                brandText={getBrandText(location.pathname)}
+                toggleSidebar={toggleSidebar}
+                sidebarOpened={sidebarOpened}
+              />
+              <Switch>
+                <Route
+                path={'/login'}
+                component={LoginPage}
+                />
+                <Redirect from="*" to="/admin/dashboard" />
+              </Switch>
+              {
+                // we don't want the Footer to be rendered on map page
+                location.pathname === "/admin/maps" ? null : <Footer fluid />
+              }
+            </div>
+          </div>
+        </React.Fragment>
+      )}
+    </BackgroundColorContext.Consumer>
+  );
+}
+
+export default Admin;
