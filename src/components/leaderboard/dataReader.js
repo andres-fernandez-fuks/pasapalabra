96cff1019ebad3368929337e0c58bf70e3e4b514
Andrés Fernández <48918488+andres912@users.noreply.github.com>
2022-07-29 16:25:50 -0300

    removes unnecesary utf8 decoding

--- src/components/leaderboard/dataReader.js
+++ src/components/leaderboard/dataReader.js
@@ -4,16 +4,9 @@ import { dbGet, dbPut } from '../../utils/dbFetcher';
 
 async function getData() {
     let data = await dbGet('scores');
-    const utf8 = require('utf8');
     const new_data = data.map(playerInfo => {
-        let name;
-        try {
-            name = utf8.decode(playerInfo[0]);
-        } catch (e) {
-            name = playerInfo[0];
-        }
         return {
-            name: name,
+            name: playerInfo[0],
             correctAnswers: playerInfo[1],
             incorrectAnswers: playerInfo[2],
             remainingTime: playerInfo[3]
