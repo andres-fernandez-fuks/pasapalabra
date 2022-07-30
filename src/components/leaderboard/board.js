8b904f71610ff8a35541e4befddc406429ce7671
Andrés Fernández <48918488+andres912@users.noreply.github.com>
2022-07-30 02:59:43 -0300

    refactors leaderboard

--- src/components/leaderboard/board.js
+++ src/components/leaderboard/board.js
@@ -1,22 +1,87 @@
 import React, { useState } from 'react'
-import Profiles from './profiles';
 import './style.css';
 import getData from './dataReader'
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function Board() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  let navigate = useNavigate();
 
   React.useEffect(() => {
     getData().then(data => {
         setData(data);
+        setLoading(false);
     }).catch(err => {
         console.log(err);
     });
   }, [])
+
+  const displayLoadingScreen = () => {
+    return (
+      <div className="loading-screen">
+        <h2>Cargando la información (puede demorar unos instantes)</h2>
+      </div>
+    )
+  }
+
     
-  console.log("Data: ", data);
+  return ( 
+    <div className="leaderboard-main-div">
+      { loading ? displayLoadingScreen() : <Leaderboard data={data}/> }
+      
+      <Button
+          className="leaderboard-back-button"
+          style={{marginTop: "25px", marginBottom: "25px"}}
+          variant="contained"
+          size="large"
+          color="warning"
+          onClick={() => {navigate("/");}}>
+          Volver
+      </Button>
+    </div>
+  )
+}
+
+function Item(data){
+  if (!data) return <></>;
   return (
-        <Profiles Leaderboard={data}></Profiles>
+      <>
+          {
+              data.map((value, index) => (
+                  <tr>
+                      <td className='td'>{value.name}</td>                
+                      <td className='td' style={{color:"darkGreen"}}>{value.correctAnswers}</td>
+                      <td className='td' style={{marginLeft:"100px", color:"darkRed"}}>{value.incorrectAnswers}</td>
+                      <td className='td' style={{marginLeft:"100px", color:"darkBlue"}}>{value.remainingTime}</td>
+                  </tr>
+                  )
+              )
+          }
+      </>
+
+      
+  )
+}
+
+function Leaderboard(props) {
+
+  return (
+    <>
+      <div className="leaderboard-table-div">
+          <h1 className='leaderboard'>Ranking</h1>
+          <table className="scoreboard-table ">
+              <tr>
+                  <th className='th'>Nombre</th>
+                  <th className='th'>Correctas</th>
+                  <th className='th'>Incorrectas</th>
+                  <th className='th'>Tiempo restante</th>
+              </tr>
+              {Item(props.data)}
+          </table>
+      </div>
+    </>
   )
 }
 
