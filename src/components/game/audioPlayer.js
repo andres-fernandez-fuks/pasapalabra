6c4d687b52b8ec42052072a74b028867ae50249b
Andrés Fernández <anfernandez@fi.uba.ar>
2024-09-12 19:43:34 -0300

    adds dev script + fixes bug of audio never stopped

--- src/components/game/audioPlayer.js
+++ src/components/game/audioPlayer.js
@@ -29,86 +29,93 @@ import happyBirthdayAudio from "../../assets/audio/happy-birthday.mp3";
 import beatlesSong from "../../assets/audio/beatles-song.mp3";
 
 const determineTrack = (letter, questionNumber) => {
-    switch (letter) {
-        case "A":
-            return track_a;
-        case "B":
-            return track_b;
-        case "C":
-            return track_c;
-        case "D":
-            return track_d;
-        case "E":
-            return track_e;
-        case "F":
-            return track_f;
-        case "G":
-            return track_g;
-        case "H":
-            return track_h;
-        case "I":
-            return track_i;
-        case "J":
-            return track_j;
-        case "L":
-            if (questionNumber === 0)
-                return track_l;
-            return track_l_alt;
-        case "M":
-            return track_m;
-        case "N":
-            return track_n;
-        case "Ñ":
-            return track_ni;
-        case "O":
-            return track_o;
-        case "P":
-            return track_p;
-        case "Q":
-            return track_q;
-        case "R":
-            return track_r;
-        case "S":
-            return track_s;
-        case "T":
-            return track_t;
-        case "U":
-            return track_u;
-        case "V":
-            return track_v;
-        case "X":
-            return track_x;
-        case "Y":
-            return track_y;
-        case "Z":
-            return track_z;
-        default:
-            return "";
-    }
-}
+  switch (letter) {
+    case "A":
+      return track_a;
+    case "B":
+      return track_b;
+    case "C":
+      return track_c;
+    case "D":
+      return track_d;
+    case "E":
+      return track_e;
+    case "F":
+      return track_f;
+    case "G":
+      return track_g;
+    case "H":
+      return track_h;
+    case "I":
+      return track_i;
+    case "J":
+      return track_j;
+    case "L":
+      if (questionNumber === 0) return track_l;
+      return track_l_alt;
+    case "M":
+      return track_m;
+    case "N":
+      return track_n;
+    case "Ñ":
+      return track_ni;
+    case "O":
+      return track_o;
+    case "P":
+      return track_p;
+    case "Q":
+      return track_q;
+    case "R":
+      return track_r;
+    case "S":
+      return track_s;
+    case "T":
+      return track_t;
+    case "U":
+      return track_u;
+    case "V":
+      return track_v;
+    case "X":
+      return track_x;
+    case "Y":
+      return track_y;
+    case "Z":
+      return track_z;
+    default:
+      return "";
+  }
+};
 
 export default function AudioPlayer(letter, questionNumber) {
   var track = determineTrack(letter, questionNumber);
   const audio = new Audio(track);
   audio.loop = false;
   return audio;
-};
+}
 
 export function SongPlayer(props) {
-    const audio = React.useMemo(() => new Audio(happyBirthdayAudio), []);
-    audio.loop = true;
+  const { audioEnabled, currentSection } = props;
+  const audio = React.useMemo(() => new Audio(happyBirthdayAudio), []);
+  audio.loop = true;
 
-    React.useEffect(() => {
-        if (props.audioEnabled) {
-            audio.play();
-        } else {
-            audio.pause();
-        }
-    }, [props.audioEnabled]);
-  };
+  React.useEffect(() => {
+    if (audioEnabled) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }, [audioEnabled]);
 
-  export function playBeatlesSong() {
-    let audio = new Audio(beatlesSong);
-    audio.loop = false;
-    audio.play();
-  };
+  React.useEffect(() => {
+    return () => {
+      audio.pause(); // Stop the audio when the component unmounts
+      audio.currentTime = 0; // Optionally, reset the audio to the start
+    };
+  }, [audio]);
+}
+
+export function playBeatlesSong() {
+  let audio = new Audio(beatlesSong);
+  audio.loop = false;
+  audio.play();
+}
--- src/pages/introductionPage.js
+++ src/pages/introductionPage.js
@@ -1,116 +1,163 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import './introductionPage.css';
+import React from "react";
+import Button from "@mui/material/Button";
+import "./introductionPage.css";
 import { withStyles } from "@material-ui/core/styles";
-import { IconButton } from '@mui/material';
-import VolumeUpIcon from '@mui/icons-material/VolumeUp';
-import VolumeOffIcon from '@mui/icons-material/VolumeOff';
+import { IconButton } from "@mui/material";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 
-import routes from '../utils/routes';
-import { useNavigate  } from "react-router-dom";
-import balloonsGif from '../assets/balloons.gif';
-import titleGif from '../assets/title.gif';
-import title2Gif from '../assets/title-2.gif';
-import { SongPlayer } from '../components/game/audioPlayer';
-import confettiGif from '../assets/confetti.gif';
-import frameImg from '../assets/frame-2.png';
+import routes from "../utils/routes";
+import { useNavigate } from "react-router-dom";
+import balloonsGif from "../assets/balloons.gif";
+import titleGif from "../assets/title.gif";
+import title2Gif from "../assets/title-2.gif";
+import { SongPlayer } from "../components/game/audioPlayer";
+import confettiGif from "../assets/confetti.gif";
+import frameImg from "../assets/frame-2.png";
 
 const PINK_BACKGOUND = "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)";
 const BLUE_BACKGROUND = "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)";
 const GREEN_BACKGROUND = "linear-gradient(45deg, #4CAF50 30%, #C8E6C9 90%)";
 
 const CustomButton = withStyles({
-    root: {
-      background: props => props.background,
-      borderRadius: 0,
-      border: 0,
-      color: "white",
-      height: 48,
-      padding: "0 30px",
-      width: "200px",
-    },
-    label: {
-      textTransform: "lowercase",
-    }
-  })(props => <Button {...props} />);
+  root: {
+    background: (props) => props.background,
+    borderRadius: 0,
+    border: 0,
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    width: "200px",
+  },
+  label: {
+    textTransform: "lowercase",
+  },
+})((props) => <Button {...props} />);
+
+export const GameSection = {
+  GAME: "game",
+  INSTRUCTIONS: "instructions",
+  LEADERBOARD: "leaderboard",
+};
 
 export default function IntroductionPage(props) {
-    const [audioEnabled, setAudioEnabled] = React.useState(false);
+  const [audioEnabled, setAudioEnabled] = React.useState(false);
+  const [currentSection, setCurrentSection] = React.useState("");
 
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const handleClick = () => {
-        setAudioEnabled(!audioEnabled);
+  const handleClick = () => {
+    setAudioEnabled(!audioEnabled);
+  };
+
+  const handleRedirect = (section) => {
+    setAudioEnabled(false);
+    setCurrentSection(section);
+    switch (section) {
+      case GameSection.GAME:
+        navigate(routes.gamePage);
+        break;
+      case GameSection.INSTRUCTIONS:
+        navigate(routes.instructionsPage);
+        break;
+      case GameSection.LEADERBOARD:
+        navigate(routes.leaderboardPage);
+        break;
+      default:
+        console.log("Invalid section");
+        break;
     }
+  };
 
-    const audioButtonDiv = () => {
-        return (
-            <div>
-                <IconButton
-                    onClick={handleClick}
-                    size="large"
-                >
-                    {audioEnabled ?
-                        <VolumeUpIcon className='audio-button'  sx={{ fontSize: "50px"}}/> :
-                        <VolumeOffIcon className='audio-button'  sx={{ fontSize: "50px"}}/>}
-                </IconButton>
-            </div>
-        )
-    }
-
+  const audioButtonDiv = () => {
     return (
-        <div className="introduction-main-div" id='main'>
-            <div className="audio-button-div">
-                {audioButtonDiv()}
-            </div>
-            <div className="balloons-div-1">
-                <img className="balloon-img" src={balloonsGif} alt="Globos" />
-            </div>
-            <div className="balloons-div-2">
-                <img className="balloon-img" src={balloonsGif} alt="Globos" />
-            </div>
-            <div className="title-1-div">
-                <img className="title-1-img" src={titleGif} alt="Título" />
-            </div>
-            <div className="title-2-div">
-                <img className="title-2-img" src={title2Gif} alt="Título" />
-            </div>
-            <div className="intro-buttons-div">
-                <CustomButton
-                    style={{ margin: "15px", color: "black", fontWeight: "bold", fontSize:"24px", fontFamily: "Papyrus", width: "300px", textTransform: "none"}}
-                    background={PINK_BACKGOUND}
-                    variant="contained"
-                    color="success"
-                    onClick={e => navigate(routes.gamePage)}
-                >
-                    Iniciar juego &nbsp;
-                </CustomButton>
-                <CustomButton
-                    style={{ margin: "15px", color: "black", fontWeight: "bold", fontSize:"24px", fontFamily: "Papyrus", width: "300px", textTransform: "none"}}
-                    background={BLUE_BACKGROUND}
-                    variant="contained"
-                    color="info"
-                    onClick={e => navigate(routes.instructionsPage)}
-                >
-                    Instrucciones &nbsp;
-                </CustomButton>
-                <CustomButton
-                    style={{ margin: "15px", color: "black", fontWeight: "bold", fontSize:"24px", fontFamily: "Papyrus", width: "300px", textTransform: "none"}}
-                    background={GREEN_BACKGROUND}
-                    variant="contained"
-                    color="warning"
-                    onClick={e => navigate(routes.leaderboardPage)}
-                >
-                    Puntajes &nbsp;
-                </CustomButton>
-            </div>
-            <div className="confetti-div">
-                <img className="confetti-img" src={confettiGif} alt="Título" />
-            </div>
-            <div className="confetti-div-2">
-                <img className="confetti-img" src={confettiGif} alt="Título" />
-            </div>
-            <SongPlayer audioEnabled={audioEnabled}/>
-        </div>
-    )
+      <div>
+        <IconButton onClick={handleClick} size="large">
+          {audioEnabled ? (
+            <VolumeUpIcon className="audio-button" sx={{ fontSize: "50px" }} />
+          ) : (
+            <VolumeOffIcon className="audio-button" sx={{ fontSize: "50px" }} />
+          )}
+        </IconButton>
+      </div>
+    );
+  };
+
+  return (
+    <div className="introduction-main-div" id="main">
+      <div className="audio-button-div">{audioButtonDiv()}</div>
+      <div className="balloons-div-1">
+        <img className="balloon-img" src={balloonsGif} alt="Globos" />
+      </div>
+      <div className="balloons-div-2">
+        <img className="balloon-img" src={balloonsGif} alt="Globos" />
+      </div>
+      <div className="title-1-div">
+        <img className="title-1-img" src={titleGif} alt="Título" />
+      </div>
+      <div className="title-2-div">
+        <img className="title-2-img" src={title2Gif} alt="Título" />
+      </div>
+      <div className="intro-buttons-div">
+        <CustomButton
+          style={{
+            margin: "15px",
+            color: "black",
+            fontWeight: "bold",
+            fontSize: "24px",
+            fontFamily: "Papyrus",
+            width: "300px",
+            textTransform: "none",
+          }}
+          background={PINK_BACKGOUND}
+          variant="contained"
+          color="success"
+          onClick={(e) => handleRedirect(GameSection.GAME)}
+        >
+          Iniciar juego &nbsp;
+        </CustomButton>
+        <CustomButton
+          style={{
+            margin: "15px",
+            color: "black",
+            fontWeight: "bold",
+            fontSize: "24px",
+            fontFamily: "Papyrus",
+            width: "300px",
+            textTransform: "none",
+          }}
+          background={BLUE_BACKGROUND}
+          variant="contained"
+          color="info"
+          onClick={(e) => handleRedirect(GameSection.INSTRUCTIONS)}
+        >
+          Instrucciones &nbsp;
+        </CustomButton>
+        <CustomButton
+          style={{
+            margin: "15px",
+            color: "black",
+            fontWeight: "bold",
+            fontSize: "24px",
+            fontFamily: "Papyrus",
+            width: "300px",
+            textTransform: "none",
+          }}
+          background={GREEN_BACKGROUND}
+          variant="contained"
+          color="warning"
+          onClick={(e) => handleRedirect(GameSection.LEADERBOARD)}
+        >
+          Puntajes &nbsp;
+        </CustomButton>
+      </div>
+      <div className="confetti-div">
+        <img className="confetti-img" src={confettiGif} alt="Título" />
+      </div>
+      <div className="confetti-div-2">
+        <img className="confetti-img" src={confettiGif} alt="Título" />
+      </div>
+      <SongPlayer audioEnabled={audioEnabled} currentSection={currentSection} />
+    </div>
+  );
 }
